e83fbf49fab05b70d461b9b53170972bae77818d
SJ- Alumno <SJ-Alumno@duoc.acad>
2026-09-22 16:40:34 -0300

    implentando react

---
+++ src/footer.jsx
@@ -0,0 +1,12 @@
+
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2026 Mi Página Web. Todos los derechos reservados.</p>
+    </footer>
+  );
+}
+
+export default Footer;
+
